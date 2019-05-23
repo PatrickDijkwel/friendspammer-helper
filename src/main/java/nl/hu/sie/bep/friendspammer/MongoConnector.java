cf785f51465e0cdd07b96594947d3d44cd6d6dcb
PatrickDijkwel <Patrick.Dijkwel@student.hu.nl>
2019-05-23 16:30:53 +0200

    Remove codesmells

--- src/main/java/nl/hu/sie/bep/friendspammer/MongoConnector.java
+++ src/main/java/nl/hu/sie/bep/friendspammer/MongoConnector.java
@@ -6,8 +6,9 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
 public class MongoConnector {
-	private final static String username = "acbbe8a16f5b9f";
-	private final static String password = "6e1a8d2b214b7c";
+	private MongoConnector() {
+		
+	}
 	
 	public static Session getSession() {
 		Properties props = new Properties();
@@ -15,13 +16,15 @@ public class MongoConnector {
 		props.put("mail.smtp.port", "2525");
 		props.put("mail.smtp.auth", "true");
 		
-		Session session = Session.getInstance(props,
+		String username = "acbbe8a16f5b9f";
+		String password = "6e1a8d2b214b7c";
+		
+		return Session.getInstance(props,
 				new javax.mail.Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
 		});
-		return session;
 	}
 }
